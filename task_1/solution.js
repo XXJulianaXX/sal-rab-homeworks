68d228e2b572ca0a4b711e88c1c5592d12b9bd80
Isaeva Julia <Chv_julia@mail.ru>
2022-04-03 19:20:35 +0500

    Discount

--- task_1/solution.js
+++ task_1/solution.js
@@ -40,7 +40,7 @@ function addInCartSumChange(sum, delta) {
 
     let newSum = oldSum + difference;
     let newSumText = newSum + ' ₽';
-    newSumText = `${newSum}  ₽`;
+    newSumText = `${newSum} ₽`;
     
     // Конец решения задания №1.3.
 
